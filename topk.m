56600093b401d36f855e5c966ea61a8d63cf5236
Zhiyuan Zou <zouzhyuan@163.com>
2018-09-22 11:25:50 +0800

    init codes

---
+++ topk.m
@@ -0,0 +1,29 @@
+function [ values, index ] = topk( A, k )
+%TOPK Summary of this function goes here
+%   Detailed explanation goes here
+dimA = size(size(A), 2);
+% fprintf('dimension: %d\n', dimA);
+
+if 2 == dimA
+    [dim1, dim2] = size(A);
+    if 1 == dim2
+        A = A';
+        [dim1, dim2] = size(A);
+    end
+    if 1 == dim1
+        tmpA = zeros(2, dim2);
+        for i = 1 : dim2
+            tmpA(1, i) = i;
+        end
+        tmpA(2, :) = A;
+    end
+end
+
+if 3 == dimA
+end
+
+[tmpValues, tmpIndex] = sort(tmpA, 2, 'descend');
+values = tmpValues(2, 1:k);
+index = tmpIndex(2, 1:k);
+
+end
